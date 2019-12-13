5ab265ccc90de82242e8912d5127306a2c34ec4c
westermann920 <42929525+westermann920@users.noreply.github.com>
2019-12-13 16:09:50 +0100

    Update ConnectionDB.cs

--- SOVA-WebApplication/SOVA-WebApplication/ConnectionDB.cs
+++ SOVA-WebApplication/SOVA-WebApplication/ConnectionDB.cs
@@ -13,21 +13,19 @@ namespace SOVA_WebApplication
         {
             DataSet ds = new DataSet();
             DataTable dt = new DataTable();
-            // PostgeSQL connection string
+            // PostgeSQL connection string (hide this in json file later)
             string connString = "Host=rawdata.ruc.dk;Username=raw5;Password=o1a1O.A);Database=raw5";
-            // Making connection with Npgsql provider
+            // Making connection with server
             NpgsqlConnection conn = new NpgsqlConnection(connString);
             conn.Open();
-            // data adapter making request from our connection
+            // data adapter to make a request to database
             NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
-            // i always reset DataSet before i do
-            // something with it.... i don't know why :-)
+            // reseting the dataSet before use XD
             ds.Reset();
-            // filling DataSet with result from NpgsqlDataAdapter
+            // filling DataSet with result from youre request to database
             da.Fill(ds);
-            // since it C# DataSet can handle multiple tables, we will select first
+            // since C# DataSets can handle multiple tables, we will select first
             dt = ds.Tables[0];
-            // since we only showing the result we don't need connection anymore
             conn.Close();
             return dt;
         }
