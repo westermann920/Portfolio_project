dc4c09ffdd0f10779197edbecd1f87c6ec2c9952
westermann920 <42929525+westermann920@users.noreply.github.com>
2020-02-10 13:22:04 +0100

    Update main.js

--- SOVA-WebApplication/SOVA-WebApplication/wwwroot/js/main.js
+++ SOVA-WebApplication/SOVA-WebApplication/wwwroot/js/main.js
@@ -1,21 +1,10 @@
-﻿var SimpleListModel = function (items) {
-    this.items = ko.observableArray(items);
-    this.itemToAdd = ko.observable("");
-    this.addItem = function () {
-        if (this.itemToAdd() != "") {
-            this.items.push(this.itemToAdd()); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
-            this.itemToAdd(""); // Clears the text box, because it's bound to the "itemToAdd" observable
-        }
-    }.bind(this);  // Ensure that "this" is always this view model
-};
-
-var SearchPosts = function (posts) {
+﻿var SearchPosts = function (posts) {
     this.posts = ko.observableArray(posts);
     this.termToFind = ko.observable("");
 
     this.getPosts = function () {
         if (this.termToFind() != "") {
-            $.getJSON("/api/post", function (data) {
+            $.getJSON("http://localhost:5000/api/post", function (data) {
                 this.posts.push(data); //adds the posts to the observableArray
                 this.termToFind("") //clears the search box
             })
