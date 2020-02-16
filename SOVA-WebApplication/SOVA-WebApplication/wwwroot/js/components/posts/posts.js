8844b02faccc21b089c8436f637c900dcad44e80
westermann920 <42929525+westermann920@users.noreply.github.com>
2020-02-16 13:12:11 +0100

    posts now supports pagination

--- SOVA-WebApplication/SOVA-WebApplication/wwwroot/js/components/posts/posts.js
+++ SOVA-WebApplication/SOVA-WebApplication/wwwroot/js/components/posts/posts.js
@@ -10,9 +10,13 @@
     // View Model
     function postViewModel() {
         var self = this;
-        self.posts = ko.observableArray();
-        self.termToFind = ko.observable("");
+        self.posts = ko.observableArray(); // Posts currently shown
+        self.allPosts = ko.observableArray(); // All posts form search
+        self.termToFind = ko.observable(""); // Search input value
+        self.setSize = ko.observable(5); // How many posts is shown at ones
+        self.currentSet = ko.observable(0); // The current post set
 
+        // Check if search input field is used or not
         self.checkTerm = function () {
             if (self.termToFind() !== "") {
                 return true;
@@ -21,18 +25,49 @@
             }
         };
 
+        // Gets data from api
         self.getPosts = async function () {
             if (self.termToFind() !== "") {
                 var response = await fetch("api/post/find/" + self.termToFind());
                 var data = await response.json();
-                self.posts.push(new PostModel(data[0]));
-                /*
-                for (var i = 0; i < 5; i++) {
-                    self.posts.push(new PostModel(data[i]));
-                }*/
-                self.termToFind(""); //clears the search box
+                for (var i = 0; i < data.length; i++) {
+                    self.allPosts.push(new PostModel(data[i]));
+                }
+                self.termToFind(""); //Clears the search box
             }
         };
+
+        // Shows the posts
+        self.page = ko.computed(function () {
+            if (self.setSize() == "all") {
+                self.posts(self.allPosts.slice(0));
+            } else {
+                var size = parseInt(self.setSize(), 10);
+                var first = size * self.currentSet(); 
+                var last = first + size;
+                self.posts(self.allPosts.slice(first, last));
+            }
+
+        }, self);
+
+        // Gets the total number of post sets
+        self.numberOfPostSets = function () {
+            var totalSets = self.allPosts().length / self.setSize() || 1;
+            return Math.ceil(totalSets);
+        }
+
+        // Gets next set posts
+        self.nextSet = function () {
+            if (self.currentSet() < self.numberOfPostSets() - 1) {
+                self.currentSet(this.currentSet() + 1);
+            }
+        }
+        // Gets previous set posts
+        self.previousSet = function () {
+            if (self.currentSet() > 0) {
+                self.currentSet(this.currentSet() - 1);
+            }
+        }
 
         /*
         self.getPosts = function () {
